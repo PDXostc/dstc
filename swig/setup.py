7ff6c723445528b4a85ec585def8b5771224b862
Erin Coffey <ecoffey1@jaguarlandrover.com>
2019-04-11 10:57:51 -0700

    pull lastest master

--- swig/setup.py
+++ swig/setup.py
@@ -6,7 +6,7 @@ setup.py file for DSTC SWIG
 
 from distutils.core import setup, Extension
 
-setup (name = 'dstc',
+setup (name = 'jlr-dstc',
        version = '0.1',
        author      = "SWIG Docs",
        description = """SWIG wrapper for DSTC.""",
