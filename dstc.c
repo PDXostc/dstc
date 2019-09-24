de0055d97f5ea1598228da96c0c1915a4a7404d7
Magnus Feuer <mfeuer1@jaguarlandrover.com>
2019-09-24 11:29:22 -0700

    Apple-specific use of PTHREAD_RECURSIVE_MUTEX_INITIALIZER

--- dstc.c
+++ dstc.c
@@ -35,7 +35,11 @@
 //
 
 dstc_context_t _dstc_default_context = {
+#ifdef __APPLE__
+    .lock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER,
+#else
     .lock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP,
+#endif
     .remote_node = { { 0, { 0 } } },
     .remote_node_ind = 0,
     .local_callback = { {0,0 } },
